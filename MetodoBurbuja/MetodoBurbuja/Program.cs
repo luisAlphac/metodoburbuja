7b39d725d144bb1835ac8b9715fb0b09a6d0f419
luisAlphac <105021585+luisAlphac@users.noreply.github.com>
2022-05-05 17:33:33 -0500

    se agrega el proyecto

---
+++ MetodoBurbuja/MetodoBurbuja/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MetodoBurbuja;
+burbuja bu=new burbuja();
+try
+{
+    bu.cargar();
+    bu.metodo();
+    bu.mostrar();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ingrese valores numéricos");
+}
---
+++ MetodoBurbuja/MetodoBurbuja/burbuja.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoBurbuja
+{
+    internal class burbuja
+    {
+        private int[] vector;
+
+        public void cargar()
+        {
+            Console.WriteLine("Método de ordenamiento de la burbuja");
+            Console.WriteLine("Ingrese la cantidad de valores a ordenar");
+            string longitud;
+            longitud = Console.ReadLine();
+            int cant;
+            cant=int.Parse(longitud);
+            vector = new int[cant];
+            for(int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine("Ingrese el valor de la posición " + (i+1) + ": ");
+                longitud=Console.ReadLine();
+                vector[i] = int.Parse(longitud);
+            }
+        }
+
+        public void metodo()
+        {
+            int t;
+            for(int a=1; a<vector.Length; a++)
+            {
+                for(int b=vector.Length-1; b>=a; b--)
+                {
+                    if(vector[b-1]>vector[b])
+                    {
+                        t=vector[b-1];
+                        vector[b-1]=vector[b];
+                        vector[b]=t;
+                    }
+                }
+            }
+        }
+
+        public void mostrar()
+        {
+            string NombreArchivo = "temp.txt";
+            //Abro el archivo
+            StreamWriter writer=File.AppendText(NombreArchivo);
+            Console.WriteLine("Números en el arreglo");
+            for(int i=0; i<vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]+" ");
+                //Ingreso información
+                writer.WriteLine(vector[i] + " ");
+            }
+            //Cierro el archivo
+            writer.Close();
+            Console.ReadKey();
+        }
+    }
+}
